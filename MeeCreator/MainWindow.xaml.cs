08f33f6c60c8cdd84c6299ab05eaab2cfd4f10eb
Mike Lang <mlang@student.neumont.edu>
2016-07-29 08:03:20 -0600

    Finally got around to committing
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ MeeCreator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeeCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bOval_Click(object sender, RoutedEventArgs e)
+        {
+            rMouth.Visibility = System.Windows.Visibility.Hidden;
+            eMouth.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void bRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            eMouth.Visibility = System.Windows.Visibility.Hidden;
+            rMouth.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
